98b7d199235ddc1d8e4e293131867a8b7cfb0650
corent01 <corentinwarin@gmail.com>
2018-03-21 21:29:02 +0100

    page historique

---
+++ historique.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>breakinghabitshackathon</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/css/styleHistorique.css">
+	<link rel="stylesheet" type="text/css" href="style/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+	<script defer src="style/fontawesome-free-5.0.4/svg-with-js/js/fontawesome-all.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row header">
+			<img class="img_ing col-md-4" src="style/img/inglogo.jpg">
+			<div class="infos_user col-md-5">
+				<h1><b>Team</b></h1>
+				<h2><b>dwich</b></h2>
+			</div>
+			<div class="solde col-md-3">
+				<h1><b>12.000€</b></h1>
+			</div>
+		</div>
+		<div class=" row onglets">
+			<div class="historique col-md-4">
+				<h1><b>Historique</b></h1>
+			</div>
+			<div class="factures col-md-4">
+				<h1><b>Factures</b></h1>
+			</div>
+			<div class="delay col-md-4">
+				<h1><b>Delay</b></h1>
+			</div>
+		</div>
+
+			<div class="container-fluid" id="historique">
+				<div id="facture_1">
+					<div class="row">
+						<div class="nom_facture col-md-6">
+							<span>Electrabel</span>
+						</div>
+						<div class="date col-md-6">
+							<span>Payé Le 21-03-2018</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="description col-md-6">
+							<span>GAZ</span>
+						</div>
+						<div class="montant col-md-6">
+							<span>50€</span>
+						</div>
+					</div>
+				</div>
+				<div id="facture_2">
+					<div class="row">
+						<div class="nom_facture col-md-6">
+							<span>PROXIMUS</span>
+						</div>
+						<div class="date col-md-6">
+							<span>Payé Le 31-03-2018</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="description col-md-6">
+							<span>Telecom</span>
+						</div>
+						<div class="montant col-md-6">
+							<span>90€</span>
+						</div>
+					</div>
+				</div>
+				<div id="facture_3">
+					<div class="row">
+						<div class="nom_facture col-md-6">
+							<span>Assurances AG</span>
+						</div>
+						<div class="date col-md-6">
+							<span>Payé Le 28-03-2018</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="description col-md-6">
+							<span>Assurance Habitation</span>
+						</div>
+						<div class="montant col-md-6">
+							<span>35€</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+	</div>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
