da38bd4f5e0a30fe41ee22aa0b66f09181a597fb
Domoso <hippolyte.malgaud@hotmail.fr>
2018-03-21 19:12:12 +0100

    Création du squelette et de son style

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>breakinghabitshackathon</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/css/style.css">
+	<link rel="stylesheet" type="text/css" href="style/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+	<script defer src="style/fontawesome-free-5.0.4/svg-with-js/js/fontawesome-all.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row header">
+			<img class="img_ing col-4 col-sm-4 col-md-4 col-lg-4" src="style/img/inglogo.jpg">
+			<div class="infos_user col-5 col-sm-5 col-md-5 col-lg-5">
+				<h1><b>Team</b></h1>
+				<h2><b>dwich</b></h2>
+			</div>
+			<div class="solde col-3 col-sm-3 col-md-3 col-lg-3">
+				<h1><b>12.000€</b></h1>	
+			</div>		
+		</div>
+		<div class=" row onglets">
+			<div class="historique col-4 colsm-4 col-md-4 col-lg-4">
+				<h1><b>Historique</b></h1>
+			</div>
+			<div class="factures col-4 colsm-4 col-md-4 col-lg-4">
+				<h1><b>Factures</b></h1>
+			</div>
+			<div class="delay col-4 colsm-4 col-md-4 col-lg-4">
+				<h1><b>Delay</b></h1>
+			</div>
+		</div>
+		<div class="row espace_factures">
+			espacefactures
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
