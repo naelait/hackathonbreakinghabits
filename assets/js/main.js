3faf959fee7a01c0e21cc29779b1909647e65c99
jordanpeter <jordanpeter26@gmail.com>
2018-03-22 05:00:38 +0100

    lol oli boulet

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(".nav-elem > a").click(function() {
-       // remove classes from all
-       $(".nav-elem > a").removeClass("nav-elem-active");
-       // add class to the one we clicked
-       $(this).addClass("nav-elem-active");
-       console.log(this)
-    });
- });
