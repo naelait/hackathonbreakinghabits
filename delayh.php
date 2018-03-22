ea150aa7fef9a624bc0a57fd82b3196753b07e35
Domoso <hippolyte.malgaud@hotmail.fr>
2018-03-22 11:05:28 +0100

    petits changements

---
+++ delayh.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>breakinghabitshackathon</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/jqueryui/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#datepicker" ).datepicker();
+        } );
+    </script>
+</head>
+<body>
+    <p>Date: <input type="text" id="datepicker"></p>
+</body>
+</html>
