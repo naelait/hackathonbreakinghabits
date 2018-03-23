b753f61678665d235e0d21dde1276cb47353923d
CorentinN <noirot.co@gmail.com>
2018-03-23 13:27:14 +0100

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,5 @@ Notification.requestPermission(function(status) {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('sw.js');
-
-    reg.pushManager.getSubscription().then(function(sub) {
-      if (sub === null) {
-        // Update UI to ask user to register for Push
-        console.log('Not subscribed to push service!');
-      } else {
-        // We have a subscription, update the database
-        console.log('Subscription object: ', sub);
-      }
-    });
-  })
-   .catch(function(err) {
-    console.log('Service Worker registration failed: ', err);
   });
 }
